3343595bb5d07ca290ffe3b0e059e6890a100cd2
AVIRAL GUPTA <91749530+aviral10x@users.noreply.github.com>
2023-09-20 02:38:17 +0530

    nft mint

---
+++ my-app/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <button className="border-white rounded-full bg-red-500 border-spacing-3 m-3 p-3">
+        One Click NFT
+      </button>
+    </main>
+  );
+}
